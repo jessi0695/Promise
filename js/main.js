91274b2bf5bf172f1faa295d62df557d4240669a
jessica <jessi0685@gmail.com>
2023-04-10 11:55:42 -0500

    Se realizaron nuevos cambios

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ getProducto();
 //Ejercicio
 
 let listaProds = document.getElementById("listaProds");
-        function createCards(prods);{
+        function createCards(prods){
             prods.forEach(prod => {
                 listaProds.insertAdjacentHTML("beforeend",
                 `<div class="card col" style="width:18rem;">
@@ -53,7 +53,7 @@ let listaProds = document.getElementById("listaProds");
                 <div class="card-body">
                 <h5 class="card-title">${prod.title}</h5>
                 <p class="card-text"><strong>${prod.category}</strong></p>
-                <p class="card-text">${prod.description.slice(0,80)}</p>
+                <p class="card-text">${prod.description.slice(0,20)}</p>
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal"
                 data-bs-target="#exampleModal"_${prod.id}">
                 Mas Info
@@ -63,16 +63,17 @@ let listaProds = document.getElementById("listaProds");
         <!--Modal-->
         <div class="modal fade" id="exampleModal_${prod.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
-    <div class="modal-dialog">
-      <div class="modal-content">
-       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel" ${prod.title}></h1>
-        <button type="button" class="btn-close" 
-        data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
+            <div class="modal-content">
+                <div class="modal-header">
+                     <h1 class="modal-title fs-5" id="exampleModalLabel" ${prod.title}></h1>
+                     <button type="button" class="btn-close" 
+                        data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+         <div class="modal-body">
+            ${prod.description}
+            <p class="text-end"><strong>$ ${prod.price} USD </strong></p>
+         </div>
+
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
@@ -83,4 +84,4 @@ let listaProds = document.getElementById("listaProds");
                 `
                 )
         }
-}
+)}
